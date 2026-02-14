3a17098273f42e016ffcaff4c4c4af3a0b3ae2c3
gurparvesh <ggrewal@americana-food.com>
2026-02-14 22:53:17 +0530

    Fix gitignore tracking

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Api.Controllers{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class AuthController : ControllerBase 
+    {
+        [HttpPost("login")]
+        public IActionResult Login(){
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("ThisIsMySuperSecretKey123456789012")
+            );
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: null,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new { token = jwt });
+
+        }
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,9 +1,62 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+
+
+var key = "ThisIsMySuperSecretKey123456789012";
+
 var builder = WebApplication.CreateBuilder(args);
+
+// adding authentication
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters{
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+    };
+});
 
 // Add services
 builder.Services.AddControllers(); // this is kinda registering the controllers - it scans Classes ending with Controller or ingerited from ControllerBase.
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
+// telling swagger that i use Jwt token 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your token."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
 
 var app = builder.Build();
 
@@ -13,6 +66,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// middlewares 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
